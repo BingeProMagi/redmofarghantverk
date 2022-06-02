83550f9ef38b3310aaa24273206eb9f5b826f387
Markus <markus.pantzar@gmail.com>
2022-06-02 07:53:20 +0200

    changed db from local to live

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `http://localhost:10013/graphql`,
+          `https://44.203.23.54/graphql`,
       },
     },
 
